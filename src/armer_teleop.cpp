f0ab2cce21bc476a72c54f6313ecd5c8e190f014
Dasun <dasun.gunasinghe@qut.edu.au>
2021-12-14 15:03:39 +1000

    calls the robot home service from the logitech middle button and uses the deadman (enable left button) to control the robot

--- src/armer_teleop.cpp
+++ src/armer_teleop.cpp
@@ -3,6 +3,7 @@
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <sensor_msgs/Joy.h>
+#include <std_srvs/Empty.h>
 
 /**
  * @brief Definition of the ArmerTeleop class TODO: move to its own header
@@ -18,11 +19,21 @@ private:
 
     ros::NodeHandle nh_;
 
+    //Control buttons/axis
     int linear_z_, linear_y_, angular_roll_, angular_pitch_;
+    //Additional Functionality Buttons
+    int deadman_btn_, home_btn_;
+    //Controller Scale4s
     double l_scale_, a_scale_;
+
+    //Other Variables
     std::string frame_id_;
+    int button_state_;
+
+    //ROS Specific
     ros::Publisher vel_pub_;
     ros::Subscriber joy_sub_;
+    ros::ServiceClient home_srv_;
 };
 
 /**
@@ -36,6 +47,8 @@ ArmerTeleop::ArmerTeleop():
     angular_pitch_(4), 
     a_scale_(0.1), 
     l_scale_(0.2),
+    deadman_btn_(4),
+    home_btn_(8),
     frame_id_("tool0")
 {
     // ------- Update class variables from ROS param server (loaded by launch file) ---------
@@ -45,15 +58,26 @@ ArmerTeleop::ArmerTeleop():
     nh_.param("axis_angular_pitch", angular_pitch_, angular_pitch_);
     nh_.param("scale_angular", a_scale_, a_scale_);
     nh_.param("scale_linear", l_scale_, l_scale_);
-    // Get node specific params (loaded by launch file)
-    nh_.getParam("frame_id", frame_id_);
+    nh_.param("enable_button", deadman_btn_, deadman_btn_);
+    nh_.param("enable_home", home_btn_, home_btn_);
 
+    // ------- Get node specific params (loaded by launch file)
+    nh_.getParam("/armer_teleop/frame_id", frame_id_);
+
+    // Defined internal states for telop: [0: idle, 1: enabled; 2: homed; 3: transition]
+    button_state_ = 0;
+
+    // ------- Debugging Outputs
     ROS_INFO_STREAM("linear scale: " << l_scale_ << " and angular scale: " << a_scale_);
     ROS_INFO_STREAM("Frame ID of robot: " << frame_id_);
 
+    // ------- Required publishers and subscribers
     vel_pub_ = nh_.advertise<geometry_msgs::TwistStamped>("arm/cartesian/velocity", 1);
     joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &ArmerTeleop::joyCallback, this);
 
+    // ------- Required Services (Home)
+    home_srv_ = nh_.serviceClient<std_srvs::Empty>("/arm/home");
+    home_srv_.waitForExistence(); //Blocks until service is available (this node should be launched after Armer)
 }
 
 /**
@@ -70,10 +94,43 @@ void ArmerTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
     twist.angular.y = (-1.0) * a_scale_ * joy->axes[angular_pitch_];
 
     geometry_msgs::TwistStamped twist_s;
-    twist_s.header.frame_id = "ee_link";
+    twist_s.header.frame_id = frame_id_;
     twist_s.twist = twist;
 
-    vel_pub_.publish(twist_s);
+    if (joy->buttons[deadman_btn_])
+    {
+        if (button_state_ == 2)
+        {
+            ROS_INFO_STREAM("SENDING EMPTY TWIST");
+            //Previously homed so send clean twist 
+            geometry_msgs::Twist empty_twist;
+            empty_twist.linear.z = 0;
+            empty_twist.linear.y = 0;
+            empty_twist.angular.z = 0;
+            empty_twist.angular.y = 0;
+            twist_s.twist = empty_twist;
+
+            //Update to idle state
+            button_state_ = 0;
+        }
+
+        //Trigger publish only if DEADMAN is pressed
+        vel_pub_.publish(twist_s);
+
+        //Update telop state
+        button_state_ = 1;
+    }
+    else if ( joy->buttons[home_btn_] )
+    {
+        ROS_INFO_STREAM("HOME CALLED....");
+        //Send robot arm to home position (defined by Armer)
+        std_srvs::Empty srv;
+        home_srv_.call(srv);
+        ROS_INFO_STREAM("HOME FINISHED....");
+
+        //Set state to HOMED
+        button_state_ = 2;
+    }
 
 }
 
